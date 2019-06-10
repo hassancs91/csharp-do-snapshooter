c6fe2fcf412f49b69b373d55b3d831091353d281
hasan aboul hasan <hasan.cs91@outlook.com>
2019-06-10 08:08:53 +0300

    Add project files.

---
+++ DO_SnapShooter/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace DO_SnapShooter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBoxKEY = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button6 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxPrice = new System.Windows.Forms.TextBox();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get Droplets";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.CheckBoxes = true;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.FullRowSelect = true;
+            this.listView1.Location = new System.Drawing.Point(12, 85);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(343, 393);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Droplet ID";
+            this.columnHeader1.Width = 163;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Droplet Name";
+            this.columnHeader2.Width = 143;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 56);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Take Snapshot";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(361, 56);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(154, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Get SnapShots by Droplet";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listView2
+            // 
+            this.listView2.CheckBoxes = true;
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6});
+            this.listView2.FullRowSelect = true;
+            this.listView2.Location = new System.Drawing.Point(361, 85);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(596, 393);
+            this.listView2.TabIndex = 5;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Snapshot ID";
+            this.columnHeader3.Width = 129;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Snapshot Name";
+            this.columnHeader4.Width = 171;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(529, 56);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(163, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Delete Selected Snapshots";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBoxKEY
+            // 
+            this.textBoxKEY.Location = new System.Drawing.Point(93, 12);
+            this.textBoxKEY.Name = "textBoxKEY";
+            this.textBoxKEY.PasswordChar = '*';
+            this.textBoxKEY.Size = new System.Drawing.Size(698, 20);
+            this.textBoxKEY.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "API KEY : ";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.BackColor = System.Drawing.Color.White;
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2,
+            this.toolStripStatusLabel3});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 481);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(969, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 9;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Created At";
+            this.columnHeader5.Width = 162;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Size";
+            this.columnHeader6.Width = 111;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(857, 56);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(100, 23);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Get ALL SnapShots";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(796, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "1 GB Price: ";
+            // 
+            // textBoxPrice
+            // 
+            this.textBoxPrice.Location = new System.Drawing.Point(857, 12);
+            this.textBoxPrice.Name = "textBoxPrice";
+            this.textBoxPrice.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPrice.TabIndex = 13;
+            this.textBoxPrice.Text = "0.05";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(196, 17);
+            this.toolStripStatusLabel1.Text = "Developed By : Hassn Aboul Hassan";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(10, 17);
+            this.toolStripStatusLabel2.Text = "|";
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.IsLink = true;
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(63, 17);
+            this.toolStripStatusLabel3.Text = "H-educate";
+            this.toolStripStatusLabel3.Click += new System.EventHandler(this.toolStripStatusLabel3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(969, 503);
+            this.Controls.Add(this.textBoxPrice);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxKEY);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Digital Ocean Snapshooter";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBoxKEY;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxPrice;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+    }
+}
+
---
+++ DO_SnapShooter/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+
+using System.Diagnostics;
+using System.Globalization;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace DO_SnapShooter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            string jsonreturn = string.Empty;
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                FileName = "crl.exe",
+                WindowStyle = ProcessWindowStyle.Hidden,
+                RedirectStandardOutput = true,
+                Arguments =
+                    "-X GET -H \"Content-Type: application/json\" -H \"Authorization: Bearer " + textBoxKEY.Text + "\" \"https://api.digitalocean.com/v2/droplets\""
+            };
+
+            using (var exeProcess = Process.Start(startInfo))
+            {
+
+                if (exeProcess != null) jsonreturn = exeProcess.StandardOutput.ReadToEnd();
+            }
+
+
+
+            dynamic dynObj = JsonConvert.DeserializeObject(jsonreturn);
+            foreach (var data in dynObj.droplets)
+            {
+
+                ListViewItem li = new ListViewItem();
+                li.Text = data.id.ToString();
+                li.SubItems.Add(data.name.ToString());
+                listView1.Items.Add(li);
+
+
+            }
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            foreach (ListViewItem item in listView1.CheckedItems)
+            {
+
+                var random = new Random();
+                int num = random.Next(1000);
+                string snapname = "h-snapshoot" + DateTime.Now.Hour.ToString() + DateTime.Now.Minute.ToString() +
+                                  DateTime.Now.Second.ToString() + DateTime.Now.Millisecond.ToString() + num.ToString();
+                ProcessStartInfo startInfo = new ProcessStartInfo
+                {
+                    CreateNoWindow = true,
+                    UseShellExecute = false,
+                    FileName = "crl.exe",
+                    WindowStyle = ProcessWindowStyle.Hidden,
+                    RedirectStandardOutput = true,
+                    Arguments =
+                        "-X POST -H \"Content-Type: application/json\" -H \"Authorization: Bearer " + textBoxKEY.Text + "\" -d \"{\\\"type\\\":\\\"snapshot\\\",\\\"name\\\":\\\"" + snapname + "\\\"}\" \"https://api.digitalocean.com/v2/droplets/" + item.Text + "/actions\""
+                };
+
+                 Process.Start(startInfo);
+
+                
+
+
+            }
+
+            MessageBox.Show("Done, Snapshots in Progress...");
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listView2.Items.Clear();
+
+            if (listView1.SelectedItems.Count < 1)
+            {
+                MessageBox.Show("Please select a Droplet");
+                return;
+            }
+
+            string jsonreturn = string.Empty;
+            var dropletId = listView1.SelectedItems[0].Text;
+
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                FileName = "crl.exe",
+                WindowStyle = ProcessWindowStyle.Hidden,
+                RedirectStandardOutput = true,
+                Arguments =
+                    "curl -X GET -H \"Content-Type: application/json\" -H \"Authorization: Bearer " + textBoxKEY.Text + "\" \"https://api.digitalocean.com/v2/droplets/" + dropletId + "/snapshots?page=1&per_page=1000"
+            };
+
+            using (var exeProcess = Process.Start(startInfo))
+            {
+
+                if (exeProcess != null) jsonreturn = exeProcess.StandardOutput.ReadToEnd();
+            }
+
+
+
+            dynamic dynObj = JsonConvert.DeserializeObject(jsonreturn);
+            foreach (var data in dynObj.snapshots)
+            {
+
+                ListViewItem li = new ListViewItem {Text = data.id.ToString()};
+                li.SubItems.Add(data.name.ToString());
+                li.SubItems.Add(data.created_at.ToString());
+                li.SubItems.Add(data.size_gigabytes.ToString() + " GB");
+                listView2.Items.Add(li);
+
+
+            }
+
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            listView2.Items.Clear();
+
+            int totalSize = 0;
+
+
+            string jsonreturn = string.Empty;
+   
+
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                FileName = "crl.exe",
+                WindowStyle = ProcessWindowStyle.Hidden,
+                RedirectStandardOutput = true,
+                Arguments =
+                    "curl -X GET -H \"Content-Type: application/json\" -H \"Authorization: Bearer " + textBoxKEY.Text + "\" \"https://api.digitalocean.com/v2/snapshots?page=1&per_page=1000000"
+            };
+
+            using (var exeProcess = Process.Start(startInfo))
+            {
+
+                if (exeProcess != null) jsonreturn = exeProcess.StandardOutput.ReadToEnd();
+            }
+
+
+
+            dynamic dynObj = JsonConvert.DeserializeObject(jsonreturn);
+            foreach (var data in dynObj.snapshots)
+            {
+
+                ListViewItem li = new ListViewItem { Text = data.id.ToString() };
+                li.SubItems.Add(data.name.ToString());
+                li.SubItems.Add(data.created_at.ToString());
+
+                int snapSize = (int) data.size_gigabytes;
+                totalSize = totalSize + snapSize;
+                li.SubItems.Add(snapSize.ToString() + " GB");
+                listView2.Items.Add(li);
+
+
+            }
+
+
+            //calculate estimate for 0.05$ per GB
+
+            double estimate = totalSize * Convert.ToDouble(textBoxPrice.Text);
+            MessageBox.Show("Estimated Monthly Costs= " + estimate.ToString(CultureInfo.InvariantCulture) + " $");
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem item in listView2.CheckedItems)
+            {
+
+                ProcessStartInfo startInfo = new ProcessStartInfo
+                {
+                    CreateNoWindow = true,
+                    UseShellExecute = false,
+                    FileName = "crl.exe",
+                    WindowStyle = ProcessWindowStyle.Hidden,
+                    RedirectStandardOutput = true,
+                    Arguments =
+                        "-X DELETE -H \"Content-Type: application/json\" -H \"Authorization: Bearer " + textBoxKEY.Text + "\" \"https://api.digitalocean.com/v2/snapshots/" + item .Text + "\""
+                };
+
+                Process.Start(startInfo);
+
+                
+
+            }
+
+            MessageBox.Show("Done, Snapshots Deletion in Progress...");
+        }
+
+        private void toolStripStatusLabel3_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://h-educate.com/");
+        }
+    }
+}
